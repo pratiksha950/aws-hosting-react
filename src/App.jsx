f6b1831cff74eb469f3166507d4a38257bd1b907
pratiksha950 <salunkepratiksha950@gmail.com>
2026-05-15 18:53:08 +0530

    require changes added:

--- src/App.jsx
+++ src/App.jsx
@@ -55,94 +55,11 @@ function App() {
         </div>
       </section>
 
-      <hr />
+  
 
-      {/* Pricing */}
-      <section>
-        <h2>Pricing Plans</h2>
-
-        <div>
-          <h3>Basic</h3>
-          <p>$5 / month</p>
-          <ul>
-            <li>1 Website</li>
-            <li>5 GB Storage</li>
-            <li>Free SSL</li>
-          </ul>
-
-          <button>Choose Plan</button>
-        </div>
-
-        <div>
-          <h3>Pro</h3>
-          <p>$15 / month</p>
-          <ul>
-            <li>10 Websites</li>
-            <li>50 GB Storage</li>
-            <li>24/7 Support</li>
-          </ul>
-
-          <button>Choose Plan</button>
-        </div>
-
-        <div>
-          <h3>Enterprise</h3>
-          <p>$30 / month</p>
-          <ul>
-            <li>Unlimited Websites</li>
-            <li>Unlimited Storage</li>
-            <li>Priority Support</li>
-          </ul>
-
-          <button>Choose Plan</button>
-        </div>
-      </section>
 
       <hr />
 
-      {/* Testimonials */}
-      <section>
-        <h2>What Users Say</h2>
-
-        <div>
-          <p>
-            "Amazing hosting platform. My website loads super fast now."
-          </p>
-
-          <h4>- Rahul</h4>
-        </div>
-
-        <div>
-          <p>
-            "Very easy deployment process and great support team."
-          </p>
-
-          <h4>- Priya</h4>
-        </div>
-      </section>
-
-      <hr />
-
-      {/* Contact */}
-      <section>
-        <h2>Contact Us</h2>
-
-        <form>
-          <input type="text" placeholder="Enter your name" />
-          <br />
-          <br />
-
-          <input type="email" placeholder="Enter your email" />
-          <br />
-          <br />
-
-          <textarea placeholder="Enter your message"></textarea>
-          <br />
-          <br />
-
-          <button type="submit">Send Message</button>
-        </form>
-      </section>
 
       <hr />
 
